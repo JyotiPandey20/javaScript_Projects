5eed2a451c66a41b1256ba6873c1ad0181ffb6c2
Jyoti Pandey <133849648+JyotiPandey20@users.noreply.github.com>
2023-10-26 14:42:17 +0530

    Add files via upload

---
+++ Temperature Converter/index.js
@@ -0,0 +1,24 @@
+const celsiusElement = document.getElementById('celsius');
+const fahrenheitElement = document.getElementById('fahrenheit');
+const kelvinElement = document.getElementById('kelvin');
+
+
+function computeTemp(event) {
+    const currentValue = +event.target.value;
+    switch (event.target.name) {
+        case "Celsius":
+            kelvinElement.value = (currentValue + 273.32).toFixed(2);
+            fahrenheitElement.value = (currentValue * 1.8 + 32).toFixed(2);
+            break;
+        case "fahrenheit":
+            celsiusElement.value = ((currentValue - 32) / 1.8).toFixed(2);
+            kelvinElement.value = ((currentValue / 32) / 1.8 + 273.32).toFixed(2);
+            break;
+        case "kelvin":
+            celsiusEl.value = (currentValue - 273.32).toFixed(2).toFixed(2);
+            fahrenheitEl.value = ((currentValue - 273.32) * 1.8 + 32).toFixed(2).toFixed(2);
+            break;
+        default:
+            break;
+    }
+}
